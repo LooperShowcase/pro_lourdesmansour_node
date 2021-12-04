b80feb643c2ff2bb34547436569173d2ba0319f3
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2021-12-04 13:55:32 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ function createUser(id, name, city) {
     city,
   };
 }
-createUser(1, "lourdes", "new york");
-createUser(2, "idk", "london");
+createUser(1, "jane", "new york");
+function readUser(id) {
+  return users[id];
+}
+
+function updateusers(id, name, city) {
+  if (users[id] == undefined) {
+    throw "ayo user is not here";
+  } else {
+    users[id] = {
+      name: name,
+      city: city,
+    };
+  }
+}
+updateusers("1", "john", "LA");
+function deleteusers(id) {
+  delete users[id];
+}
+
 console.log(users);
+let dataToString = JSON.stringify(users);
+fs.writeFileSync("db.json", dataToString);
